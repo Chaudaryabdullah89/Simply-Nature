1a65fd57d157bd5b95489f95a0f284f1fdb91b07
Chaudaryabdullah <chaudaryabdullah899@gmail.com>
2024-11-22 09:10:47 -0800

    ADSF

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -22,28 +22,28 @@ const Products = () => {
     fetchproduct();
   }, [productid, products]);
   return productdata ? (
-    <div>
-      <div className="border-t-2 pt-10 mt-10 transition-all ease-in duration-500 text-start max-w-[1280px] mx-auto ">
+    <div className=" max-w-[1280px] mx-auto">
+      <div className="border-t-2 pt-10 mt-10 transition-all ease-in duration-500 text-center ">
         <div className="flex flex-col md:flex-row gap-12 ">
           {/* product image */}
           <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
-            <div className="flex flex-col w-[25%] sm:w-40">
+            <div className="flex ml-8 sm:ml-0 flex-col  w-[35%] sm:w-40">
               {productdata.image.map((item, index) => (
                 <img
                   onClick={() => setimage(item)}
                   src={item}
                   key={index}
-                  className="  sm:mb-3 flex-shrink-0 cursor-pointer "
+                  className="    sm:mb-3 flex-shrink-0 cursor-pointer "
                   alt=""
-                />
+                /> 
               ))}
             </div>
-            <div className="w-[80%]  flex-shrink-0 cursor-pointer">
-              <img src={image} alt="" />
+            <div className="w-[80%]  items-center sm:mx-auto mx-auto flex-shrink-0 cursor-pointer">
+              <img src={image} alt="" className="rounded-lg" />
             </div>
           </div>
           {/* Dertailes loading */}
-          <div className="flex-col flex gap-2 text-start w-[36%] ">
+          <div className="flex-col w-full flex gap-2 text-start sm:w-[36%] px-5 sm:pl-0 ">
             <h1 className="font-semibold text-2xl ">{productdata.name}</h1>
             <div className="flex items-center gap-1 mt-2">
               <img src={assets.star_icon} alt="" className="w-3 5" />
@@ -85,8 +85,28 @@ const Products = () => {
         </div>
       </div>
       {/* des */}
-      <div className="" >
-        
+      <div className="flex flex-col mt-10 ">
+        <div className="flex flex-row mx-auto sm:mx-0">
+          <h2 className="text-lg border py-2 px-3 font-semibold">
+            Description
+          </h2>
+          <h2 className="text-lg border py-2 px-3 font-semibold">
+            Reviews(541)
+          </h2>
+        </div>
+        <div className="border px-4 py-8 flex flex-col gap-5 text-gray-500" >
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, enim.
+            Cum culpa dignissimos deleniti, quos nobis ipsam vel repellat animi
+            voluptatum quaerat, in sunt eum eius magnam facere aut dicta!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+            tenetur illum error cupiditate enim, sint amet suscipit similique
+            rem et consequuntur aspernatur dolor. Eaque, sint enim quo officia
+            minus quisquam.
+          </p>
+        </div>
       </div>
     </div>
   ) : (
